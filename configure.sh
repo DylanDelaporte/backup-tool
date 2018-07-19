ae4004d32ebeb12a48c981f1705c69fd8b9d11f6
Dylan Delaporte <dylan.delaporte06@gmail.com>
2018-07-19 02:14:49 +0000

    fixed bug with cron

--- configure.sh
+++ configure.sh
@@ -19,7 +19,7 @@ chmod +x ${path}/backup-tool/start-backup
 echo "Backup scheduled each day at 1am"
 
 crontab -l > mycron
-echo "0 1 * * * sh ${path}/backup-tool/start-backup" >> mycron
+echo "0 1 * * * bash ${path}/backup-tool/start-backup" >> mycron
 
 crontab mycron
 rm mycron
